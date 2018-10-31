f0149f9a3f74934506116b81e9f6a5bc73bd2625
xiaorui <rxiao@hpe.com>
2018-10-31 18:25:07 +0800

    submit autoConvert

---
+++ main/autoConvert.py
@@ -0,0 +1,33 @@
+#coding=utf-8
+import time
+from pic2word import picture2words
+import json
+import clipboard
+from PIL import ImageGrab as ig;
+from PIL import Image as image; 
+import cStringIO
+
+if __name__=="__main__":
+    key=dict()
+    settings=open('../res/settings.properties','r')
+    for line in settings:
+        key.update({line.split('=')[0]:line.split('=')[1]})
+    settings.close()
+
+    try:
+        while True:
+            picture=ig.grabclipboard()
+            if image.isImageType(picture):
+                
+                buffer = cStringIO.StringIO()
+                picture.save(buffer, format="JPEG")
+                result=picture2words(buffer.getvalue())
+                result_str=u"".encode("UTF-8")
+                print(result)
+                for res in json.loads(result)["words_result"]:
+                    result_str+=res["words"]
+                print(result_str)
+                clipboard.copy(result_str)
+            time.sleep(2)
+    except KeyboardInterrupt:
+        exit()
--- main/pic2word.py
+++ main/pic2word.py
@@ -26,3 +26,25 @@ def pic2word(pic_location):
     if (content):
         return content
     return 
+def picture2words(image):
+    # getToken key
+    key=dict()
+    keyFile=open('../res/keys.properties','r')
+    for line in keyFile:
+        key.update({line.split('=')[0]:line.split('=')[1]})
+    keyFile.close()
+    access_token=getToken(key["APIKey"].strip('\n'),key["SecretKey"].strip('\n'))
+    url = 'https://aip.baidubce.com/rest/2.0/ocr/v1/general?access_token=' + json.loads(access_token)['access_token']
+    # 二进制方式打开图文件
+    #f = open(pic_location, 'rb')
+    # 参数image：图像base64编码
+    img = base64.b64encode(image)
+    params = {"image": img}
+    params = urllib.urlencode(params)
+    request = urllib2.Request(url, params)
+    request.add_header('Content-Type', 'application/x-www-form-urlencoded')
+    response = urllib2.urlopen(request)
+    content = response.read()
+    if (content):
+        return content
+    return 
